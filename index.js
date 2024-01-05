77733699c897ff1f9380b6d0b9dce8a7a8583dde
PrathameshKhile <pvkhile@gmail.com>
2024-01-06 02:19:07 +0530

    created homepage and added a button to log in page.

---
+++ index.js
@@ -0,0 +1,17 @@
+function login()
+        {   
+            var uname=document.getElementById('username').value;
+            var pass=document.getElementById('password').value;
+            if(uname=='User1' && pass=='123')
+            {
+                location.assign("pk.html");
+            }
+            else{
+                window.alert("You are not an Avenger");
+            }
+        }
+        function Cancel()
+        {
+            document.getElementById('username').value="";
+            document.getElementById('password').value="";
+        }
