3a5e92160373b8a2be6dd98d7c153aa8d2e12ccd
Achintya Choudhari <122711576+AchintyaCh@users.noreply.github.com>
2024-01-06 11:36:42 +0530

    Update forget.js

--- forget.js
+++ forget.js
@@ -1,1 +1,18 @@
-
+function login()
+        {   
+            var uname=document.getElementById('username').value;
+            var pass=document.getElementById('password').value;
+            var pass1=pass;
+            if(uname=='User1' && pass==pass1;)
+            {
+                location.assign("pk.html");
+            }
+            else{
+                window.alert("You Have Successfully Logged In");
+            }
+        }
+        function Cancel()
+        {
+            document.getElementById('username').value="";
+            document.getElementById('password').value="";
+        }
