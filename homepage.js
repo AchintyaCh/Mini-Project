2b8df5e5da397444f8549a53bdb2fadf8a3b6833
PrathameshKhile <pvkhile@gmail.com>
2024-01-10 23:13:36 +0530

    added animations

--- homepage.js
+++ homepage.js
@@ -1,4 +1,26 @@
 function loginRedirect(){
-   window.location.href = 'login.html';
+   document.querySelector(".login").classList.add('login_new');
 
+}
+function home(){
+   document.querySelector(".login").classList.remove('login_new');
+
+}
+
+function login()
+{
+    var uname=document.getElementById('username').value;
+    var pass=document.getElementById('password').value;
+    if(uname=='User1' && pass=='123')
+    {
+        location.assign("pk.html");
+    }
+    else{
+        window.alert("You are not an Avenger");
+    }
+}
+function Cancel()
+{
+    document.getElementById('username').value="";
+    document.getElementById('password').value="";
 }
